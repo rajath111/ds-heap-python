3245b5cae47527a6a504ce9e321b0e28ab078d2e
Rajath E S <rajathes999@gmail.com>
2023-10-31 19:03:43 +0530

    Updated client

--- client.py
+++ client.py
@@ -3,14 +3,11 @@ from src.max_heap import MaxHeap
 
 
 if __name__ == '__main__':
-    # # Let's create a simple complete tree
-
+    # Using Max heap to sort array in descending order
     heap = MaxHeap()
     heap.insert(10)
     heap.insert(23)
     heap.insert(5)
-
-    # assert heap.get_max() == 23
 
     heap.insert(100)
     heap.insert(12)
